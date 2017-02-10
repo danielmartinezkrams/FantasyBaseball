3462893df2a6127831b72173b0758d14d15bf7cb
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-02-10 13:49:26 -0800

    Albinson - Fixed batting.json file.  Had bad data at end of file.

--- js/script.js
+++ js/script.js
@@ -2,30 +2,47 @@
  * Created by h205p2 on 2/6/17.
  */
 function nameChange(){
-    document.getElementById("name").value = "";
+    if(document.getElementById("name").value == "Enter Last Name Here"){
+        document.getElementById("name").value = "";
+    }
 }
 $(document).ready(function(){
-    $('output').hide();
+    /*$('output').empty();
     $('name').hover(function(){
         $(this).remove();
     });
+    */
     $("button").click(function(){
         $('output').show();
-        $.getJSON("Database/master.json", function(result) {
+        $.getJSON("Database/master.json", function(result){
+
+            //iterate over items in master
             $.each(result, function (key, value) {
+
+                //if last name matches
                 if($("#name").val() == value.nameLast) {
+                    console.log(" value: " + value);
                     $("#output").append(value.NameFirst + " " + value.nameLast + "<br>");
+
+                    //iterate over items in pitching
                     $.getJSON("Database/pitching.json", function(pResult) {
                         $.each(pResult, function (pKey, pValue) {
                             if(value.playerID == pValue.playerID) {
+                                console.log(" pKey: " + pKey + " pValue: " + pValue);
                                 $("#output").append(pValue.yearID+" "+pValue.teamID+" "+pValue.ERA+" "+pValue.W+" "+pValue.L+" "+pValue.BAOpp+"<br>");
+                                console.log(pValue.yearID+" "+pValue.teamID+" "+pValue.ERA+" "+pValue.W+" "+pValue.L+" "+pValue.BAOpp+"<br>");
                             }
                         });
                     });
-                    $.getJSON("Database/batting.json", function(bResult) {
+                    //end pitching iterate
+
+                    console.log("hey");
+                    $.getJSON("Database/batting.json", function(bResult){
+                        console.log("hi");
                         $.each(bResult, function (bKey, bValue) {
                             if(value.playerID == bValue.playerID) {
                                 $("#output").append(bValue.yearID+" "+bValue.teamID+" "+bValue.G+" "+bValue.H+" "+bValue.AB+" "+(bValue.H)/(bValue.AB)+"<br>");
+                                console.log(bValue.yearID+" "+bValue.teamID+" "+bValue.G+" "+bValue.H+" "+bValue.AB+" "+(bValue.H)/(bValue.AB))
                             }
                         });
                     });
